22c765ab234addd2bfad9327633ca22d00b01b56
Svetlana <dedyanatalya@yandex.ru>
2020-09-25 15:40:11 +0500

    all

---
+++ 11/11/11.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n; 
+	sum = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int x;
+		cin >> x;
+		sum = sum + x;
+	}
+	cout << sum << endl;
+	return 0;
+
+}
+
+
